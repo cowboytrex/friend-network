6ee253b82699cabfe04573c47d0158c0d5931faf
cowboytrex <careesechow@gmail.com>
2024-08-21 06:47:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+// routes/index.js
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const thoughtRoutes = require('./thoughtRoutes');
+const apiRoutes = require('./api');
 
-router.use('/users', userRoutes);
-router.use('/thoughts', thoughtRoutes);
+// Use API routes
+router.use('/api', apiRoutes);
+
+// Default response for root URL
+router.get('/', (req, res) => {
+  res.send('Welcome to the Social Network API!');
+});
+
+// Catch all other routes not defined
+router.use((req, res) => res.send('Wrong route!'));
 
 module.exports = router;
