1ab852ff75af5352ddeaab54919534b716b2ab4c
cowboytrex <careesechow@gmail.com>
2024-08-21 06:44:39 +0800

    Update userRoutes.js

--- userRoutes.js
+++ userRoutes.js
@@ -6,16 +6,11 @@ const {
   updateUser,
   deleteUser,
   addFriend,
-  removeFriend
+  removeFriend,
 } = require('../../controllers/userController');
 
-// /api/users
 router.route('/').get(getUsers).post(createUser);
-
-// /api/users/:userId
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
-
-// /api/users/:userId/friends/:friendId
 router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 module.exports = router;
