be7768ea5bec7befd05222b3bc6af9eb5c2bc6ab
cowboytrex <careesechow@gmail.com>
2024-08-21 06:47:15 +0800

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/socialNetworkDB');
+// Set Mongoose strictQuery setting
+mongoose.set('strictQuery', false); // or true, depending on your preference
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetworkDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = mongoose.connection;
